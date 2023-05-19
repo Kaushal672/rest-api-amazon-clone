9f53e9b8f111125e6c24f09e5b2ca38d7ecb20af
Kaushal <kbhandary672@gmail.com>
2023-05-19 11:39:55 +0530

    add error handling

--- app.js
+++ app.js
@@ -7,9 +7,18 @@ const app = express();
 
 app.use('/products', productsRoute);
 
+app.use((err, req, res, _next) => {
+    const { statusCode = 500 } = err;
+    // eslint-disable-next-line no-param-reassign
+    if (!err.message) err.message = 'Something went wrong, Try again!';
+    res.status(statusCode).json({ message: err.message });
+});
+
 async function main() {
     await mongoose.connect(process.env.DB_URL);
-    app.listen(process.env.PORT);
+    app.listen(process.env.PORT, () =>
+        console.log('App listening on port 8080')
+    );
 }
 
 main().catch((err) => console.log('ERROR', err));
