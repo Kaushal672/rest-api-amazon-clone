ee8a134d6db7dca1795c27b50e4974f7ec702732
Kaushal <kbhandary672@gmail.com>
2023-05-25 09:57:11 +0530

    add response headers

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization '
+    );
+    next();
+});
+
 app.use('/products', productsRoute);
 
 app.use((err, req, res, _next) => {
