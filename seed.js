d7e272ffc02e056f1249af0f8b1bc8ef953b7b2e
Kaushal <kbhandary672@gmail.com>
2023-08-04 18:11:26 +0530

    add seed file for mongodb

---
+++ seed.js
@@ -0,0 +1,289 @@
+const products = [
+    {
+        title: 'OnePlus Bullets Z2 Bluetooth Wireless in Ear Earphones with Mic, Bombastic Bass - 12.4 mm Drivers, 10 Mins Charge - 20 Hrs Music, 30 Hrs Battery Life, IP55 Dust and Water Resistant (Magico Black)',
+        description:
+            'A quick 10-minute charge delivers up to 20 hours of immersive audio playback. The flagship-level battery life delivers up to 30 hours of non-stop music on a single charge. A large 12.4 mm bass driver delivers uncompromisingly deep bass for powerful beats. Experience incredibly rich audio detail at every frequency with the titanium coating dome. Anti-distortion audio technology ensures your audio playtime stays silky-smooth. Thanks to the larger sound cavity and industry-leading algorithms, notes remain distortion-free. Water and sweat-resistant, the IP55-rated internals and design ensure your OnePlus Bullets Wireless Z2 stay all-weather ready.',
+        price: 3000,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/e_background_removal/f_png/v1690614222/Amazon-Products/51UhwaQXCpL._SX522__orz8cu.jpg',
+                filename: 'Amazon-Products/51UhwaQXCpL._SX522__orz8cu.jpg',
+            },
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/e_background_removal/f_png/v1690614242/Amazon-Products/51tdhnb9hBL._SX522__f4kt1p.jpg',
+                filename: 'Amazon-Products/51tdhnb9hBL._SX522__f4kt1p.jpg',
+            },
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690614256/Amazon-Products/51LkC94UraL._SX522__slxldu.jpg',
+                filename: 'Amazon-Products/51LkC94UraL._SX522__slxldu.jpg',
+            },
+        ],
+        offer: 'Best seller',
+        category: 'Electronics',
+        discount: 13,
+    },
+    {
+        title: 'crocs Unisex-Adult Bayaband Clog Bpk/Cpk Clog',
+        description:
+            'Sole: Rubber. Closure: Pull On. Shoe Width: Medium. Outer Material: Rubber. Closure Type: Slip On. Toe Style: Round Toe. Warranty Type: Manufacturer. Warranty Description: 30 days',
+        price: 8000,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690613806/Amazon-Products/j8tlqydzuuinxkyfeh9p.jpg',
+                filename: 'Amazon-Products/j8tlqydzuuinxkyfeh9p.jpg',
+            },
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690613846/Amazon-Products/71-k9kCycgL._UX575__hu1fdd.jpg',
+                filename: 'Amazon-Products/71-k9kCycgL._UX575__hu1fdd.jpg',
+            },
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690613870/Amazon-Products/71kx7MJqYQL._UX575__wyj4rd.jpg',
+                filename: 'Amazon-Products/71kx7MJqYQL._UX575__wyj4rd.jpg',
+            },
+        ],
+        offer: 'Best seller',
+        category: 'Clothing & Accessories',
+        discount: 67,
+    },
+    {
+        title: 'Apple iPhone 14 Pro Max (1 TB)',
+        description:
+            '17.00 cm (6.7-inch) Super Retina XDR display featuring Always-On and ProMotion. Dynamic Island, a magical new way to interact with iPhone. 48MP Main camera for up to 4x greater resolution. Cinematic mode now in 4K Dolby Vision up to 30 fps. Action mode for smooth, steady, handheld videos. All-day battery life and up to 29 hours of video playback. Vital safety technology — Crash Detection can detect a severe car crash and call for help.',
+        price: 189900,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690611603/apple-iphone-14-pro-max-1_dybk2q.jpg',
+                filename: 'apple-iphone-14-pro-max-1_dybk2q.jpg',
+            },
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690611548/photo-1664478546384-d57ffe74a78c_fcrguc.jpg',
+                filename: 'photo-1664478546384-d57ffe74a78c_fcrguc.jpg',
+            },
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690611624/apple-iphone-14-pro-4_r960fp.jpg',
+                filename: 'apple-iphone-14-pro-4_r960fp.jpg',
+            },
+        ],
+        offer: 'Best seller',
+        category: 'Electronics',
+        discount: 6,
+    },
+    {
+        title: 'Safari Thorium Sharp Antiscratch Trolley Bag Medium Size, 66 cms Black Printed Hard Side Travel Bag for Men and Women, 4 Wheel Luggage Suitcase for Travelling',
+        description:
+            'Presenting Thorium Antiscratch – A combination of durability and design that is crafted to add Style and elegance to your every move! The interiors are designed to offer maximum packing space and the Fixed lock will ensure safety of all your belongings. So get ready to give traveling in style a new meaning. Pattern: Solid.',
+        price: 2999,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1684990820/Amazon-Products/qfygp4xlxjohk9nkwyz8.png',
+                filename: 'Amazon-Products/qfygp4xlxjohk9nkwyz8.png',
+            },
+        ],
+        offer: "Today's deals",
+        category: 'Travelling',
+        discount: 56,
+    },
+    {
+        title: 'Fire-Boltt Phoenix Pro 1.39" Bluetooth Calling Smartwatch, AI Voice Assistant, Metal Body with 120+ Sports Modes, SpO2, Heart Rate Monitoring (Silver Grey)',
+        description:
+            '【1.39 Bigger Round Display】- Comes with a 1.39 TFT Color Full Touch Screen and a 240*240 Pixel High Resolution this watch is covered to flaunt the sleek looks with a 280 NITS peak brightness. The watch will work on a single charge for about 7 days (without Bluetooth calling) and about 4 Days with Bluetooth calling. Charging Specs - The watch needs to be charged for 3 hours to reach 100%. The charger should be a 3.7V to 5V adapter or any laptop output. For a bare minimum of 20% charge the watch needs to be charged for about 30-40 mins. You cannot store Music in the watch, you can only control the music. 【120+ Sports Modes】- Track each activity effectively with this smartwatch & activity tracker. Track your calories, steps and much more while you are on your fitness journey. This fitness tracker has it all. There is no volume control, however when connected to bt calling you can control the volume of the call. 【Metal Body】- Fire-Boltt Phoenix Pro possess a metal body that is durable and long lasting, The watch gives a sleek look and gloss finish with shiny and is anti-corrosive. Supported Applications - Notifcations from all social media channels (Instagram, Whatsapp, Facebook), Call Notifications, Health Tracking (SpO2, Heart Rate, Sleep), Sports Tracking & many more. 【AI Voice Assistant】- The smartwatch comes with a AI Voice assistant that can with one tap wake up Siri/Google on your phone and you can give the commands to get your work accomplished.',
+        price: 11999,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690613358/Amazon-Products/fire-boltt-phoenix-p_ayx6un.webp',
+                filename: 'Amazon-Products/fire-boltt-phoenix-p_ayx6un.webp',
+            },
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690613377/Amazon-Products/fire-boltt-phoenix-p_zlrjd2.webp',
+                filename: 'Amazon-Products/fire-boltt-phoenix-p_zlrjd2.webp',
+            },
+        ],
+        offer: 'Best seller',
+        category: 'Electronics',
+        discount: 87,
+    },
+    {
+        title: 'Sony PS5 PlayStation Console',
+        description:
+            "Maximize your play sessions with near instant load times for installed PS5 games. The custom integration of the PS5 console's systems lets creators pull data from the SSD so quickly that they can design games in ways never before possible. Immerse yourself in worlds with a new level of realism as rays of light are individually simulated, creating true-to-life shadows and reflections in supported PS5 games. Play your favorite PS5 games on your stunning 4K TV. Enjoy smooth and fluid high frame rate gameplay at up to 120fps for compatible games, with support for 120Hz output on 4K displays. With an HDR TV, supported PS5 games display an unbelievably vibrant and lifelike range of colors. PS5 consoles support 8K Output, so you can play games on your 4320p resolution display.",
+        price: 55000,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690612285/Amazon-Products/photo-1606144042614-b2417e99c4e3_bnn6nm.jpg',
+                filename:
+                    'Amazon-Products/photo-1606144042614-b2417e99c4e3_bnn6nm.jpg',
+            },
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690612264/Amazon-Products/photo-1607853202273-797f1c22a38e_ehopfy.jpg',
+                filename:
+                    'Amazon-Products/photo-1607853202273-797f1c22a38e_ehopfy.jpg',
+            },
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690612315/Amazon-Products/photo-1607016284318-d1384bf5edd1_irfv2c.jpg',
+                filename:
+                    'Amazon-Products/photo-1607016284318-d1384bf5edd1_irfv2c.jpg',
+            },
+        ],
+        offer: 'Best seller',
+        category: 'Electronics',
+        discount: 14,
+    },
+    {
+        title: 'OnePlus Nord CE 3 Lite 5G (Pastel Lime, 8GB RAM, 128GB Storage)',
+        description:
+            'Camera: 108 MP Main Camera with EIS; 2MP Depth-Assist Lens and 2MP Macro Lens; Front (Selfie) Camera: 16MP. Rear Camera Mode: Hi-res 108MP mode, 3x Lossles Zoom, Photo, Video, Nightscape, Expert, Panoramic, Portrait, Macro, Time-lapse, Slow-motion, Long exposure, Dual-view video, Text Scanner, 1080p/720p@30fps, Video zoom: 1080P@30fps, 720P@30fps, Slow motion: 720P@120fps, Time-Lapse: 1080P@30fps, Steady Video EIS support. Display: 6.72 Inches; 120 Hz Adaptive refresh rate; FHD+(1080×2400), Colors: 16.7 Million colors, 391 ppi, Aspect Ratio: 20:9, Brightness: 550 nits (typical), 680 nits (HBM). Operating System: Oxygen OS based on Android 13.1. Processor: Qualcomm Snapdragon 695 5G. Battery & Charging: 5000 mAh with 67W SUPERVOOC Endurance Edition.',
+        price: 19999,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690612904/Amazon-Products/oneplus-nord-ce-3-lite-1_u3d0bm.jpg',
+                filename: 'Amazon-Products/oneplus-nord-ce-3-lite-1_u3d0bm.jpg',
+            },
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1690612921/Amazon-Products/oneplus-nord-ce-3-lite-2_iqjfvs.jpg',
+                filename: 'Amazon-Products/oneplus-nord-ce-3-lite-2_iqjfvs.jpg',
+            },
+        ],
+        offer: 'Best seller',
+        category: 'Electronics',
+        discount: 0,
+    },
+    {
+        title: 'Bajaj DX-6 1000W Dry Iron with Advance Soleplate and Anti-bacterial German Coating Technology, White',
+        description:
+            '360 Degrees swivel cord.American heritage coating non stick sole plate. Dual color. Safety plus thermal fuse. Other Comfort Features: Multiple Temperature Levels. 2 years on product. Indicator light for on and off. Automatic Shut Down and Temperature Control. Cord length of 1.7m',
+        price: 575,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1684990823/Amazon-Products/dtr8cobbunkacdc9mnkr.png',
+                filename: 'Amazon-Products/dtr8cobbunkacdc9mnkr.png',
+            },
+        ],
+        offer: "Today's deals",
+        category: 'Home Appliance',
+        discount: 27,
+    },
+    {
+        title: 'Zebronics HAZE Wireless mouse for Computers, Laptops with 1200 DPI, Advanced optical sensor, 2.4GHz USB Nano receiver, Plug - Play usage, Power saving mode and Comfortable use on most surfaces - Black.',
+        description:
+            'Wireless mouse for office use, student use & everyday desktop purposes. Compatible with desktop computers, laptops | Plug & Play usage without drivers. 1200 DPI | Easy & Effortless control | Comfortable for editing, documentation, browsing & other regular use cases. Advanced Optical Sensor | 3 Button control | Durable performance | 1 million click cycles.',
+        price: 299,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1684990827/Amazon-Products/f7ko1zd6ujoubp9qtt7n.png',
+                filename: 'Amazon-Products/f7ko1zd6ujoubp9qtt7n.png',
+            },
+        ],
+        offer: "Today's deals",
+        category: 'Electronics',
+        discount: 40,
+    },
+    {
+        title: 'Fastrack ColorFit Pulse Grand smart watch with 1.69" HD display, 60 sports modes, 150 watch faces, fast charge, Spo2, Stress, Sleep, Heart Rate Monitoring & IP68 Waterproof - Jet Black',
+        description:
+            'Get the rich immersive viewing expeirience on the 1.69" LCD display. Now enjoy more than a day\'s worth/25 hours of battery in just 15 minutes of charge. Get valuable insights about your health from Fastrack Health Suite featuring 24*7 heart rate, stress, blood oxygen, sleep monitor. Stay in touch with world quick replies available for Android.',
+        price: 1299,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1684990829/Amazon-Products/sf6luk3mzmonfeiiqvqu.png',
+                filename: 'Amazon-Products/sf6luk3mzmonfeiiqvqu.png',
+            },
+        ],
+        offer: "Today's deals",
+        category: 'electronics',
+        discount: 68,
+    },
+    {
+        title: 'pTron Bassbuds Duo in ear earbuds with 32Hrs Total Playtime, Bluetooth 5.1 wireless headphones, stereo audio, touch control TWS, with mic, Type-C fast charging, IPX4 & voice Assistance',
+        description:
+            "True Wireless Bluetooth Headphones(TWS) with immersive stereo sound. deep bass , smart touch control & 32 Hours of Total Playtime with the charging case. Advanced Bluetooth v5.1 with strong 10M wireless connectivity, 13MM large dynamic driver. Quick connect , Auto Reconnect. Widely compatible with bluetooth enabled devices. Ergonomatic & Lightweight wireless earbuds, snug-fit design built in HD Mic for clear call quality. Water/SweatProof, Instant Access to Device's Voice Assistant.",
+        price: 899,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1684990829/Amazon-Products/qm8vjyxzfmq11qwjxv3h.png',
+                filename: 'Amazon-Products/qm8vjyxzfmq11qwjxv3h.png',
+            },
+        ],
+        offer: "Today's deals",
+        category: 'electronics',
+        discount: 65,
+    },
+    {
+        title: 'OnePlus 6 (Mirror Black, 8GB RAM, 128GB Storage)',
+        description:
+            '20 + 16 MP Dual rear camera with optical image stabilzation, super slow motion, portrait mode | 16 MP front camera. 15.95 centimeters (6.28-inch) Full HD+ Optic Amoled display with 2280x1080 pixels and 19:9 aspect ratio | 2.5D corning gorilla glass 5. Dual Nano sim with dual standby (4G + 4G).',
+        price: 34999,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1684990829/Amazon-Products/u4ubfsjdmee8hx65r9lv.png',
+                filename: 'Amazon-Products/u4ubfsjdmee8hx65r9lv.png',
+            },
+        ],
+        offer: "Today's deals",
+        category: 'electronics',
+        discount: 48,
+    },
+    {
+        title: 'Safari Spartan 21 Ltrs Water resistant backpack - Blue, S',
+        description:
+            'Lighweight yet durable backpack/daypack for school use or everyday outing. Front storage pocket keeps smaller items neatly organized and easy to access. Mesh pockets on both sides to accomodate your water bottle and umbrella. Mesh Padding on back & Shoulder strap to provide comfort & better back support.',
+        price: 599,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1684990830/Amazon-Products/qlypuuntzpa6dzxklhyy.png',
+                filename: 'Amazon-Products/qlypuuntzpa6dzxklhyy.png',
+            },
+        ],
+        offer: "Today's deals",
+        category: 'Travelling',
+        discount: 70,
+    },
+    {
+        title: 'Green soul raptor racing edition ergonomic gaming chair with premium PU Leather, Adjustable neck & Lumbar Pillow, 2D Adjustable Armrests & Heavy Duty Nylon Base.',
+        description:
+            'Racing seat design coupled with thigh support side bolsters help ace the long day at work or play. The chair is specially designed keeping ergonomic comfort in mind. The premium PU leather upholstery, sturdy metal frame coupled with soft molded foam in the seat and back help you maintain pefect posture.',
+        price: 13999,
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dlds2z087/image/upload/v1684990831/Amazon-Products/jidwryw7sqow8u9sqsve.png',
+                filename: 'Amazon-Products/jidwryw7sqow8u9sqsve.png',
+            },
+        ],
+        offer: "Today's deals",
+        category: 'Office Products',
+        discount: 42,
+    },
+];
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+const Product = require('./model/products');
+const Review = require('./model/reviews');
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+    console.log('Database connected');
+}
+
+main().catch((err) => console.log('Error', err));
+
+const seedDB = async () => {
+    await Product.deleteMany({});
+    await Review.deleteMany({});
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const prod of products) {
+        const product = new Product({
+            title: prod.title,
+            description: prod.description,
+            images: prod.images,
+            price: prod.price,
+            offer: prod.offer,
+            discount: prod.discount,
+            category: prod.category,
+            seller: new mongoose.Types.ObjectId('64b3f0bf38b4d04adaa09889'),
+        });
+        await product.save();
+    }
+};
+seedDB().then(() => {
+    mongoose.connection.close();
+});
