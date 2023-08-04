26a89f2d777730b5da78f487c5bcb5ae4258e5d2
Kaushal <kbhandary672@gmail.com>
2023-08-04 18:45:16 +0530

    modify get all products query

--- controllers/products.js
+++ controllers/products.js
@@ -2,15 +2,9 @@ const Product = require('../model/products');
 const { cloudinary } = require('../cloudinary/index');
 
 exports.getProducts = async (req, res) => {
-    const { page = 1 } = req.query;
-    const perPage = 2;
-    const count = await Product.find().countDocuments();
+    const products = await Product.find().sort({ createdAt: -1 });
 
-    const products = await Product.find()
-        .sort({ createdAt: -1 })
-        .skip((page - 1) * perPage)
-        .limit(perPage);
-    res.status(200).json({ products, count });
+    res.status(200).json({ products });
 };
 
 exports.addProduct = async (req, res) => {
